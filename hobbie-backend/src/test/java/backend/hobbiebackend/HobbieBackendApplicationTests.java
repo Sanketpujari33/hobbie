8428fe601ea7ea3c69eea9a2b902f75a4f5a790f
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-05-05 15:10:34 +0530

    initial commit

--- hobbie-backend/src/test/java/backend/hobbiebackend/HobbieBackendApplicationTests.java
+++ hobbie-backend/src/test/java/backend/hobbiebackend/HobbieBackendApplicationTests.java
@@ -1,13 +1,11 @@
-package com.example.demo;
+package backend.hobbiebackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HobbieBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
